7da8134f197a27db9d0bb9d3b13c16b6ecb70f9a
Julia <jjuliaborgess@gmail.com>
2025-09-24 15:37:57 -0300

    csv

--- codiguin/src/services/cadastroService.ts
+++ codiguin/src/services/cadastroService.ts
@@ -18,8 +18,7 @@ export function cadastrarCliente(clienteAtual: Cliente): Cliente {
     historicoPedidos: [],
   };
 
-  // Aqui você pode adicionar código para gravar no cadastro.csv
   console.log(`✅ Cliente ${novoCliente.nome} cadastrado com sucesso!`);
-
+const CAMINHO_CSV_PEDIDOS = "csv/cadastro.csv";
   return novoCliente;
 }
